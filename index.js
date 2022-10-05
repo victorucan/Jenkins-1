fc2ec852e371401ad1e82ef481b85d28d75f76a1
victorucan <88557060+victorucan@users.noreply.github.com>
2022-10-05 12:14:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use('/persona', personaRoute);
 
 app.get('/', (req, res) => {
-  res.send("hola mundos");
+  res.send("resultado exitoso");
 });
 
 app.listen(port, () => {
